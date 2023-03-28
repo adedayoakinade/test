d8d2a6e0aae8045c8e3019fd5c2a771f194330bc
Adedayo Akinade <akinadeadedayo1@gmail.com>
2023-03-28 07:17:24 -0700

    Commit

---
+++ printf_percent.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * printf_percent - prints the char 37 which is the ASCII for percent sign.
+ * Return: 1.
+ */
+int printf_percent(void)
+{
+	_putchar(37);
+	return (1);
+}
